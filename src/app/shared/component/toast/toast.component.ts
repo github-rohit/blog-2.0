eb1a6b8b2e24f8f71630d4daa269c357c2e19d25
Rohit Kumar Gautam <nirmal.rohit@yahoo.co.in>
2018-01-26 20:22:41 +0530

    fix

--- src/app/shared/component/toast/toast.component.ts
+++ src/app/shared/component/toast/toast.component.ts
@@ -6,12 +6,11 @@ import { Component, OnInit, Input, Output } from '@angular/core';
   styleUrls: ['./toast.component.css']
 })
 export class ToastComponent {
-  @Input('toast') toast: object;
+  @Input('toast') toast: any;
 
   constructor() { }
 
   close() {
-    console.log('close');
     this.toast = {};
   }
 
